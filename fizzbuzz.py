1431eacbb288990f8c7b12601438f2ef7b2f64d9
hiasha1 <95436084+hiasha1@users.noreply.github.com>
2024-07-22 21:15:16 -0400

    FizzBuzz code added

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,11 @@
 # add your code here
+# FizzBuzz
+for num in range(1, 101):
+    if num % 3 == 0 and num % 5 == 0 :
+        print (num , "FizzBuzz")
+    elif num % 3 == 0 :
+        print (num , "Fizz")
+    elif num % 5 == 0 :
+        print (num , "Buzz")
+
 
